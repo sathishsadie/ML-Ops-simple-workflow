89de4ae3064021126d797563e9532191c78fdfcf
Sathish. G <sadie26032005@gmail.com>
2024-09-11 22:39:28 +0530

    small changes

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -4,7 +4,8 @@ import pandas as pd
 from src.ingestion import DataIngestion
 
 def test_data_ingestion():
-    data_ingestion = DataIngestion(r'artifacts/train.csv')  # Use a sample data file path
+    data_ingestion = DataIngestion('artifacts\\train.csv')  # Use a sample data file path
     df = data_ingestion.read()
     assert isinstance(df, pd.DataFrame)
     assert not df.empty
+test_data_ingestion()
